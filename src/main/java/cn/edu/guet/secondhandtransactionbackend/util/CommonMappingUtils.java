9135cdbebdbd58d2ba28aee7f98a80e2243adf95
Sammy <caxapokrq@gmail.com>
2025-07-31 18:47:57 +0800

    支持多文件上传并接入S3存储，完善文件上传接口及文档

--- src/main/java/cn/edu/guet/secondhandtransactionbackend/util/CommonMappingUtils.java
+++ src/main/java/cn/edu/guet/secondhandtransactionbackend/util/CommonMappingUtils.java
@@ -1,6 +1,7 @@
 package cn.edu.guet.secondhandtransactionbackend.util;
 
 import org.mapstruct.Named;
+
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
@@ -12,6 +13,7 @@ public class CommonMappingUtils {
     public static URI toUri(String url) {
         return url == null ? null : URI.create(url);
     }
+
 
     @Named("fromUri")
     public static String fromUri(URI uri) {
